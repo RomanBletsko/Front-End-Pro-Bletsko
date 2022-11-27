70788e0a87f712e1a024225066c29dd8ef28b6ac
RomanBletsko <romanblecko@gmail.com>
2022-11-27 16:25:22 +0200

    hw17 done

--- hw17/index.js
+++ hw17/index.js
@@ -1,6 +1,6 @@
-const API_Key = "uYHPCrdlZ6Nwi2puAgv2HJwcYCEf5Jr5";
+const API_Key = "K0TNJSRxnVYuWmnzFDRK57OXKpkdcBcw";
 const API_URL = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/";
-const sitys = [
+const citys = [
   { name: "Lviv", key: 324561 },
   { name: "Kyiv", key: 324505 },
   { name: "Kharkiv", key: 323903 },
@@ -15,104 +15,262 @@ const sitys = [
   { name: "New York", key: 349727 },
 ];
 
-const sity = document.querySelector(".chosen_city");
-sitys.forEach((element) => {
+const city = document.querySelector(".chosen_city");
+citys.forEach((element) => {
   const option = document.createElement("option");
   option.innerHTML = element.name;
-  sity.append(option);
+  city.append(option);
 });
 
-let sityKey;
+let cityKey;
+let cityName;
 const button = document.querySelector(".btn");
 button.addEventListener("click", () => {
-  sitys.forEach((element) => {
-    if (element.name === sity.value) {
-      sityKey = element.key;
+  citys.forEach((element) => {
+    if (element.name === city.value) {
+      cityKey = element.key;
+      cityName = element.name;
     }
   });
 });
+const outpuCityName = function () {
+  const cityNameWrapper = document.querySelector(".chosen-city-wrapp");
+  cityNameWrapper.replaceChildren();
+  const citySpan = document.createElement("span");
+  citySpan.innerHTML = cityName;
+  citySpan.classList.add("city-span");
+
+  cityNameWrapper.append(citySpan);
+};
+const renderError = () => {
+  document.querySelector(".day-list").replaceChildren();
+  document.querySelector(".neighbors-list").replaceChildren();
+  const errorHolder = document.querySelector(".result-wrapper");
+  errorHolder.replaceChildren();
+  const errorSpan = document.createElement("span");
+  errorSpan.innerHTML = "Error something is wrong!";
+  errorSpan.classList.add("error-span");
+
+  errorHolder.append(errorSpan);
+};
+// I tried to do something like this but it doesn't work!
+// const buildPaletWeather = (element) => {
+// const elementsArr = [
+//   {
+//     name: "day",
+//     type: "li",
+//     class: "day-item",
+//     target: "day-list",
+//   },
+//   {
+//     name: "wrapper1",
+//     type: "div",
+//     class: "wrapper1",
+//     target: "day-item",
+//   },
+//   {
+//     name: "wrapper2",
+//     type: "div",
+//     class: "wrapper2",
+//     target: "day-item",
+//   },
+//   {
+//     name: "spanDay",
+//     type: "span",
+//     value: "Day:",
+//     class: "span",
+//     target: "wrapper1",
+//   },
+//   {
+//     name: "spanNight",
+//     type: "span",
+//     value: "Night:",
+//     class: "span",
+//     target: "wrapper2",
+//   },
+//   {
+//     name: "skyDay",
+//     type: "span",
+//     class: "span",
+//     // value: element.Day.IconPhrase,
+//     target: "wrapper1",
+//   },
+//   {
+//     name: "skyNigh",
+//     type: "span",
+//     class: "span",
+//     // value: element.Day.IconPhrase,
+//     target: "wrapper2",
+//   },
+// ];
+//   let counter = 0;
+//   elementsArr.forEach((obj) => {
+//     const el = document.createElement(obj.type);
+//     el.classList.add(obj.class);
+//     document.querySelector(`.${obj.target}`).append(el);
+//   });
+// };
+
+const findWeekday = (date) => {
+  let weekday;
+  const dateNum = date.getDay();
+  switch (dateNum) {
+    case 0:
+      weekday = "Sunday";
+      break;
+    case 1:
+      weekday = "Monday";
+      break;
+    case 2:
+      weekday = "Tuesday";
+      break;
+    case 3:
+      weekday = "Wednesday";
+      break;
+    case 4:
+      weekday = "Thursday";
+      break;
+    case 5:
+      weekday = "Friday";
+      break;
+    case 6:
+      weekday = "Suturday";
+      break;
+    default:
+      break;
+  }
+  return weekday;
+};
+const dayList = document.querySelector(".day-list");
+const loadWeatherData = (result) => {
+  const daysArray = result.DailyForecasts;
+  dayList.replaceChildren();
+  daysArray.forEach((element) => {
+    const day = document.createElement("li");
+    day.classList.add("day-item");
+
+    const wrapper1 = document.createElement("div");
+    const wrapper2 = document.createElement("div");
+    wrapper1.classList.add("wrapper");
+    wrapper2.classList.add("wrapper");
+
+    const spanDay = document.createElement("span");
+    const spanNight = document.createElement("span");
+    spanDay.innerHTML = "Day:";
+    spanNight.innerHTML = "Night:";
+
+    const skyDay = document.createElement("span");
+    const skyNight = document.createElement("span");
+    skyDay.innerHTML = element.Day.IconPhrase;
+    skyNight.innerHTML = element.Night.IconPhrase;
+    const precipitationDay = document.createElement("span");
+    const precipitationNight = document.createElement("span");
+    precipitationDay.innerHTML = element.Day.HasPrecipitation
+      ? element.Day.PrecipitationType
+      : "Without precipitation.";
+    precipitationNight.innerHTML = element.Night.HasPrecipitation
+      ? element.Night.PrecipitationType
+      : "Without precipitation.";
+
+    const iconDay = document.createElement("span");
+    const iconNight = document.createElement("span");
+    iconDay.classList.add("icon");
+    iconNight.classList.add("icon");
+    iconDay.innerHTML = `<img src="./icons/${element.Day.Icon}-s.png" alt="image"/>`;
+    iconNight.innerHTML = `<img src="./icons/${element.Night.Icon}-s.png" alt="image"/>`;
+    const date1 = new Date(element.Date);
+    const date = document.createElement("span");
+    date.classList.add("date");
+
+    const weekdayOutput = document.createElement("span");
+    weekdayOutput.classList.add("weekday");
+    weekdayOutput.innerHTML = findWeekday(date1);
+    date.innerHTML = `${date1.getDate()}-${date1.getMonth()}-${date1.getFullYear()}`;
+    day.append(weekdayOutput, date);
+
+    const tempDay = document.createElement("span");
+    const tempNight = document.createElement("span");
+    tempDay.classList.add("tempDay");
+    tempNight.classList.add("tempNight");
+    tempDay.innerHTML = `${convertToCelsius(
+      element.Temperature.Maximum.Value
+    )} ℃`;
+
+    tempNight.innerHTML = `${convertToCelsius(
+      element.Temperature.Minimum.Value
+    )} ℃`;
+    wrapper1.append(spanDay, tempDay, iconDay, skyDay, precipitationDay);
+    wrapper2.append(
+      spanNight,
+      tempNight,
+      iconNight,
+      skyNight,
+      precipitationNight
+    );
+    day.append(wrapper1, wrapper2);
+    dayList.append(day);
+  });
+};
 const convertToCelsius = function (temp) {
   return Math.round((temp - 32) / 1.8);
 };
-const dayList = document.querySelector(".day-list");
-const xhr = new XMLHttpRequest();
-const loadWeather = function () {
-  const fullUrl = `${API_URL}${sityKey}?apikey=${API_Key}`;
-  xhr.open("GET", fullUrl);
-  xhr.send();
-  xhr.onload = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      const result = JSON.parse(this.response);
-      const daysArray = result.DailyForecasts;
-      dayList.replaceChildren();
-      daysArray.forEach((element) => {
-        const day = document.createElement("li");
-        day.classList.add("day-item");
+const neighbors = document.querySelector(".neighbors-list");
+const buildNeighbor = (result) => {
+  neighbors.replaceChildren();
+  for (let i = 0; i < 5; i++) {
+    const neighborItem = document.createElement("li");
+    neighborItem.classList.add("neighbors-item");
+    const neighborName = document.createElement("span");
+    neighborName.classList.add("neghbor-city");
+    const getWeathertBtn = document.createElement("button");
+    getWeathertBtn.classList.add("neighbor-Btn");
+    getWeathertBtn.innerHTML = "Get weather";
+    getWeathertBtn.id = result[i].Key;
+    neighborName.innerHTML = result[i].EnglishName;
+    neighborItem.id = result[i].EnglishName;
 
-        const wrapper1 = document.createElement("div");
-        const wrapper2 = document.createElement("div");
-        wrapper1.classList.add("wrapper");
-        wrapper2.classList.add("wrapper");
-        wrapper2.classList.add("wrapper1");
-
-        const spanDay = document.createElement("span");
-        const spanNight = document.createElement("span");
-        spanDay.innerHTML = "Day:";
-        spanNight.innerHTML = "Night:";
-
-        const skyDay = document.createElement("span");
-        const skyNight = document.createElement("span");
-        skyDay.innerHTML = element.Day.IconPhrase;
-        skyNight.innerHTML = element.Night.IconPhrase;
-        const precipitationDay = document.createElement("span");
-        const precipitationNight = document.createElement("span");
-        precipitationDay.innerHTML = element.Day.HasPrecipitation
-          ? element.Day.PrecipitationType
-          : "Without precipitation.";
-        precipitationNight.innerHTML = element.Night.HasPrecipitation
-          ? element.Night.PrecipitationType
-          : "Without precipitation.";
-
-        const iconDay = document.createElement("span");
-        const iconNight = document.createElement("span");
-        iconDay.classList.add("icon");
-        iconNight.classList.add("icon");
-        iconDay.innerHTML = `<img src="./icons/${element.Day.Icon}-s.png" alt="image"/>`;
-        iconNight.innerHTML = `<img src="./icons/${element.Night.Icon}-s.png" alt="image"/>`;
-        const date1 = new Date(element.Date);
-        const date = document.createElement("span");
-        date.classList.add("date");
-        date.innerHTML = `${date1.getDate()}-${date1.getMonth()}-${date1.getFullYear()}`;
-        day.append(date);
-
-        const tempDay = document.createElement("span");
-        const tempNight = document.createElement("span");
-        tempDay.classList.add("tempDay");
-        tempNight.classList.add("tempNight");
-        tempDay.innerHTML = `${convertToCelsius(
-          element.Temperature.Maximum.Value
-        )} ℃`;
-
-        tempNight.innerHTML = `${convertToCelsius(
-          element.Temperature.Minimum.Value
-        )} ℃`;
-        wrapper1.append(spanDay);
-        wrapper1.append(tempDay);
-        wrapper1.append(iconDay);
-        wrapper1.append(skyDay);
-        wrapper1.append(precipitationDay);
-        wrapper2.append(spanNight);
-        wrapper2.append(tempNight);
-        wrapper2.append(iconNight);
-        wrapper2.append(skyNight);
-        wrapper2.append(precipitationNight);
-        day.append(wrapper1);
-        day.append(wrapper2);
-        dayList.append(day);
-      });
-    } else {
-      console.log("No data from server");
-    }
-  };
+    neighborItem.append(neighborName, getWeathertBtn);
+    neighbors.append(neighborItem);
+  }
 };
-button.addEventListener("click", loadWeather);
+const loadWeather = async () => {
+  try {
+    const response = await fetch(`${API_URL}${cityKey}?apikey=${API_Key}`);
+    if (response.status === 200) {
+      const result = await response.json();
+      loadWeatherData(result);
+      outpuCityName();
+    }
+  } catch (error) {
+    console.error(error);
+    renderError();
+  }
+};
+const neighborsBaseURL =
+  "http://dataservice.accuweather.com/locations/v1/cities/neighbors/";
+const loadNeighbors = async () => {
+  try {
+    const response = await fetch(
+      `${neighborsBaseURL}${cityKey}?apikey=${API_Key}`
+    );
+    if (response.status === 200) {
+      const result = await response.json();
+      buildNeighbor(result);
+    }
+  } catch (error) {
+    console.error(error);
+    renderError();
+  }
+};
+button.addEventListener("click", () => {
+  loadWeather();
+  loadNeighbors();
+});
+neighbors.addEventListener("click", (event) => {
+  if (event.target.classList.contains("neighbor-Btn")) {
+    cityKey = event.target.id;
+    loadWeather();
+    loadNeighbors();
+  }
+  cityName = event.path[1].id;
+});
